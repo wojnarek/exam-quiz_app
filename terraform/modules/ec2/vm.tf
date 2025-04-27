2c1b2f9f3dddc9d0b9a8d84a48e5e9c13d2fd24d
wojnarek <wojnarek99@gmail.com>
2025-04-27 11:03:24 +0200

    deleted rule allow to connect via ssh (was for testing)

--- terraform/modules/ec2/vm.tf
+++ terraform/modules/ec2/vm.tf
@@ -12,15 +12,6 @@ resource "aws_security_group" "app_sg" {
     security_groups = var.security_group_lb_id
   }
 
-  ingress {
-    description = "ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
   egress {
     from_port   = 0
     to_port     = 0
